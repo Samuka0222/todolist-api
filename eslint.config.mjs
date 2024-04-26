653ab88437f85daba433aa2fb9134c231039ded3
Samuka0222 <mmachado0222@gmail.com>
2024-04-26 14:16:13 -0300

    refactor: changes on eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,17 @@ import globals from "globals";
 
 import { FlatCompat } from "@eslint/eslintrc";
 import pluginJs from "@eslint/js";
+import prettier from "eslint-config-prettier";
 import path from "path";
 import { fileURLToPath } from "url";
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended });
 
 export default [
-  {languageOptions: { globals: globals.node }},
+  { languageOptions: { globals: globals.node } },
   ...compat.extends("standard-with-typescript"),
   {
     rules: {
@@ -21,8 +22,10 @@ export default [
         "ignoreMemberSort": false,
         "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
         "allowSeparatedGroups": false
-    }],
-
+      }],
+    },
+    plugins: {
+      prettier
     }
   }
 ];
