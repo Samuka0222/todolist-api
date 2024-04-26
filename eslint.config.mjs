cc33acdb679899d3a908450b350e99d1a59e01e2
Samuka0222 <mmachado0222@gmail.com>
2024-04-25 21:50:04 -0300

    chore: add initial linting setup

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from "globals";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+
+export default [
+  {languageOptions: { globals: globals.node }},
+  ...compat.extends("standard-with-typescript"),
+  {
+    rules: {
+      "sort-imports": ["error", {
+        "ignoreCase": false,
+        "ignoreDeclarationSort": false,
+        "ignoreMemberSort": false,
+        "memberSyntaxSortOrder": ["none", "all", "multiple", "single"],
+        "allowSeparatedGroups": false
+    }],
+
+    }
+  }
+];
