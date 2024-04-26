ea8b9216d5640cedc13f8220c4981b6c867a5333
Samuka0222 <mmachado0222@gmail.com>
2024-04-26 14:23:54 -0300

    feat: ensure that user can create a new account

---
+++ src/controllers/SignUpController.ts
@@ -0,0 +1,47 @@
+import { ZodError, z } from 'zod';
+import { AccountAlreadyExists } from '../errors/AccountAlreadyExists';
+import { IController, IRequest, IResponse } from '../interfaces/IController';
+import { SignUpUseCase } from '../useCases/SignUpUseCase';
+
+const schema = z.object({
+  name: z.string().min(2),
+  email: z.string().email(),
+  password: z.string().min(8),
+});
+
+export class SignUpController implements IController {
+  constructor(private readonly signUpUseCase: SignUpUseCase) {}
+  // Inversão de dependecia, para não criar uma instância de SignUpUseCase, fazemos com que o signUpUseCase seja acionado quando o controller precisar
+
+  async handle({ body }: IRequest): Promise<IResponse> {
+    try {
+      const { name, email, password } = schema.parse(body);
+      await this.signUpUseCase.execute({ name, email, password });
+
+      return {
+        statusCode: 204,
+        body: null,
+      };
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return {
+          statusCode: 400,
+          body: {
+            message: error.issues,
+          },
+        };
+      }
+
+      if (error instanceof AccountAlreadyExists) {
+        return {
+          statusCode: 409,
+          body: {
+            error: 'This e-mail is already in use.',
+          },
+        };
+      }
+
+      throw error;
+    }
+  }
+}
