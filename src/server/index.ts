283ee2e45c0febba4280133c430b2c09f27dba3e
Samuka0222 <mmachado0222@gmail.com>
2024-04-30 10:22:06 -0300

    feat: add cors and multiple origins for the API

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,17 @@
+import cors from 'cors';
 import express from 'express';
 import { accountsRouter } from './routes/accountsRoutes';
 import { authRouter } from './routes/authRoutes';
 import { tasksRouter } from './routes/tasksRoutes';
 
+const origins = ['http://localhost:3000', 'http://localhost:3001'];
+
+const corsOptions = {
+  origin: origins,
+};
+
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json(), authRouter, accountsRouter, tasksRouter);
 
 app.listen(3001, () => {
